efc13484733daeab5ecd43bd79661740a0048937
fatalryuu <vanyax941@gmail.com>
2022-12-11 16:11:42 +0300

    css changes

--- 01-first-project/react-kabzda-1/src/components/Navbar/Navbar.jsx
+++ 01-first-project/react-kabzda-1/src/components/Navbar/Navbar.jsx
@@ -9,11 +9,11 @@ const Navbar = (props) => {
             <div className={s.item}>
                 <NavLink to="/profile" className = {({isActive}) => isActive ? s.active : s.item}>Profile</NavLink>
             </div>
-            <div className={`${s.item} ${s.active}`}>
-                <NavLink to="/dialogs" className = {({isActive}) => isActive ? s.active : s.item}>Messages</NavLink>
-            </div>
             <div className={s.item}>
                 <NavLink to="/news" className = {({isActive}) => isActive ? s.active : s.item}>News</NavLink>
+            </div>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to="/dialogs" className = {({isActive}) => isActive ? s.active : s.item}>Messages</NavLink>
             </div>
             <div className={s.item}>
                 <NavLink to="/music" className = {({isActive}) => isActive ? s.active : s.item}>Music</NavLink>
