0c3bd61d955e44dc9306802ff737fefd3cf33b78
fatalryuu <vanyax941@gmail.com>
2022-12-14 10:06:01 +0300

    lesson 61

--- 01-first-project/react-kabzda-1/src/App.js
+++ 01-first-project/react-kabzda-1/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import './App.css';
-import Header from "./components/Header/Header";
-import Profile from "./components/Profile/Profile";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
@@ -10,16 +8,17 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import SidebarContainer from "./components/Sidebar/SidebarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = () => {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
-                <Header />
+                <HeaderContainer />
                 <SidebarContainer />
                 <div className='app-wrapper-content'>
                     <Routes>
-                        <Route path={process.env.PUBLIC_URL} element={<Profile />}/>
+                        <Route path={process.env.PUBLIC_URL} element={<ProfileContainer isMain={true}/>}/>
                         <Route path='/profile' element={<ProfileContainer isMain={true}/>}/>
                         <Route path='/profile/:userID' element={<ProfileContainer />}/>
                         <Route path='/news' element={<News />}/>
--- 01-first-project/react-kabzda-1/src/components/Header/Header.jsx
+++ 01-first-project/react-kabzda-1/src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import s from './Header.module.css'
 import icon from '../../img/icon.png'
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
-            <a href="/profile"><img src={icon} alt=""/></a>
+            <NavLink to='/profile'><img src={icon} alt=""/></NavLink>
+            <div className={s.login}>
+                {props.isAuth ? props.login : <NavLink to='/login'>Login</NavLink>}
+            </div>
         </header>
     );
 };
