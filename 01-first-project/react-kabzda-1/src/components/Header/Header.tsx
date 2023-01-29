3891c3be0a3237e50dd51af1f3d1a43f65eb7776
fatalryuu <vanyax941@gmail.com>
2023-01-29 17:16:27 +0300

    TS Header

--- 01-first-project/react-kabzda-1/src/components/Header/Header.tsx
+++ 01-first-project/react-kabzda-1/src/components/Header/Header.tsx
@@ -3,7 +3,13 @@ import s from './Header.module.css'
 import icon from '../../img/icon.png'
 import {NavLink} from "react-router-dom";
 
-const Header = (props) => {
+type PropsType = {
+    isAuth: boolean
+    login: string | null
+    logout: () => void
+}
+
+const Header: React.FC<PropsType> = (props) => {
     return (
         <header className={s.header}>
             <NavLink to='/profile'><img src={icon} alt=""/></NavLink>
