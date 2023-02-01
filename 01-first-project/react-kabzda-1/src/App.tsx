56c3af908884377a89598b84892509223891d995
fatalryuu <vanyax941@gmail.com>
2023-02-01 18:42:43 +0300

    app -> fc

--- 01-first-project/react-kabzda-1/src/App.tsx
+++ 01-first-project/react-kabzda-1/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Suspense, lazy} from 'react'
+import React, {lazy, Suspense, useEffect} from 'react'
 import './App.css';
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
@@ -8,68 +8,53 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import UsersPage from "./components/Users/UsersContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
-import {compose} from "@reduxjs/toolkit";
 import {initializeApp} from "./redux/appReducer";
-import {connect} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import Preloader from "./components/Common/Preloader/Preloader";
-import withRouter from "./components/Common/withRouter/withRouter";
 import Page from "./components/Common/Page/Page";
-import {AppStateType} from "./redux/store";
-
+import {AppDispatch, AppStateType} from "./redux/store";
 
 const DialogsContainer = lazy(() => import('./components/Dialogs/DialogsContainer'));
 const ProfileContainer = lazy(() => import('./components/Profile/ProfileContainer'));
 
-type PropsType = MapStatePropsType & MapDispatchPropsType;
+const App: React.FC = () => {
+    const dispatch: AppDispatch = useDispatch();
+    useEffect(() => {
+        dispatch(initializeApp());
+    }, []);
 
-type MapStatePropsType = {
-    initialized: boolean
-}
+    const initialized = useSelector((state: AppStateType) => state.app.initialized);
 
-type MapDispatchPropsType = {
-    initializeApp: () => void
-}
+    if (!initialized)
+        return <Preloader/>
 
-class App extends React.Component<PropsType> {
-    componentDidMount() {
-        this.props.initializeApp();
-    }
-
-    render() {
-        if (!this.props.initialized)
-            return <Preloader/>
-        return (
-            <BrowserRouter>
-                <div className='app-wrapper'>
-                    <HeaderContainer/>
-                    <Sidebar/>
-                    <div className='app-wrapper-content'>
-                        <Suspense fallback={<Preloader/>}>
-                            <Routes>
-                                <Route path='/Social-Network' element={<Page title="Profile"><ProfileContainer/></Page>}/>
-                                <Route path='/profile' element={<Page title="Profile"><ProfileContainer/></Page>}/>
-                                <Route path='/profile/:userID' element={<Page title="Profile"><ProfileContainer/></Page>}/>
-                                <Route path='/news' element={<Page title="News"><News/></Page>}/>
-                                <Route path='/dialogs/*' element={<Page title="Dialogs"><DialogsContainer/></Page>}/>
-                                <Route path='/users' element={<Page title="Users"><UsersPage/></Page>}/>
-                                <Route path='/music' element={<Page title="Music"><Music/></Page>}/>
-                                <Route path='/settings' element={<Page title="Settings"><Settings/></Page>}/>
-                                <Route path='/login' element={<Page title="Login"><Login/></Page>}/>
-                            </Routes>
-                        </Suspense>
-                    </div>
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <HeaderContainer/>
+                <Sidebar/>
+                <div className='app-wrapper-content'>
+                    <Suspense fallback={<Preloader/>}>
+                        <Routes>
+                            <Route path='/Social-Network' element={<Page title="Profile"><ProfileContainer/></Page>}/>
+                            <Route path='/' element={<Page title="Profile"><ProfileContainer/></Page>}/>
+                            <Route path='/profile' element={<Page title="Profile"><ProfileContainer/></Page>}/>
+                            <Route path='/profile/:userID' element={<Page title="Profile"><ProfileContainer/></Page>}/>
+                            <Route path='/news' element={<Page title="News"><News/></Page>}/>
+                            <Route path='/dialogs/*' element={<Page title="Dialogs"><DialogsContainer/></Page>}/>
+                            <Route path='/users' element={<Page title="Users"><UsersPage/></Page>}/>
+                            <Route path='/music' element={<Page title="Music"><Music/></Page>}/>
+                            <Route path='/settings' element={<Page title="Settings"><Settings/></Page>}/>
+                            <Route path='/login' element={<Page title="Login"><Login/></Page>}/>
+                            <Route path='*' element={<Page title="Not Found">
+                                <div style={{color: "white", fontSize: 72}}>404 NOT FOUND</div>
+                            </Page>}/>
+                        </Routes>
+                    </Suspense>
                 </div>
-            </BrowserRouter>
-        )
-    };
+            </div>
+        </BrowserRouter>
+    )
 }
 
-const mapStateToProps = (state: AppStateType): MapStatePropsType => {
-    return {
-        initialized: state.app.initialized
-    }
-}
-
-export default compose<React.Component>(
-    withRouter,
-    connect<MapStatePropsType, MapDispatchPropsType, {}, AppStateType>(mapStateToProps, {initializeApp}))(App);
+export default App;
