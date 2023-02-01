78014e6c14ec08e46deff70aae48460dc62f3515
Ivan <92427151+fatalryuu@users.noreply.github.com>
2023-02-01 22:18:45 +0300

    all delete

--- 01-first-project/react-kabzda-1/src/utils/objectHelper.ts
+++
@@ -1,7 +0,0 @@
-export const updateObjectInArray = (items: Array<any>, itemID: number, property: string, newObjProps: Object) => {
-    return items.map(el => {
-        if (el[property] === itemID)
-            return {...el, ...newObjProps}
-        return el;
-    })
-}
