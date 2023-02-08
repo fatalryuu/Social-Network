cd903d8c0a0c1cf8c211425964a16b9492a42176
fatalryuu <vanyax941@gmail.com>
2023-02-08 18:51:52 +0300

    redirect from chat page

--- src/components/ChatPage/ChatPage.tsx
+++ src/components/ChatPage/ChatPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Chat from "./Chat/Chat";
+import {useSelector} from "react-redux";
+import {AppStateType} from "../../redux/store";
+import {Navigate} from "react-router-dom";
 
 const ChatPage: React.FC = () => {
-    return (
-        <Chat />
-    );
+    const isAuth = useSelector((state: AppStateType) => state.auth.isAuth);
+    return <>
+        {!isAuth ? <Navigate to={'/login'}/> : <Chat />}
+    </>
 };
 
 export default ChatPage;
